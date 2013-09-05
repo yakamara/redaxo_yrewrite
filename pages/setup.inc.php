b33edff51e88a3fe921573b034b5ccc27c162c9e
Jan Kristinus <jan.kristinus@yakamara.de>
2013-09-05 14:17:00 +0200

    einfachste hilfetexte eingebaut.

--- pages/setup.inc.php
+++ pages/setup.inc.php
@@ -13,14 +13,73 @@ if ($func != '') {
     }
 }
 
+$domains = array();
+
+foreach(rex_yrewrite::$domainsByName as $name => $val) {
+    if($name != 'undefined') {
+        $domains[] = '<tr><td><a href="http://'.$name.'">'.htmlspecialchars($name).'</a></td><td><a href="http://'.$name.'/sitemap.xml">sitemap.xml</a></td><td><a href="http://'.$name.'/robots.txt">robots.txt</a></td></tr>';
+    }
+}
+
 echo '
+
+            <style>
+             .rex-area .rex-area-content p.rex-tx1 code{
+              background-color:#dFe9e9;
+              padding:10px;
+              display:block
+            }
+
+            .rex-area .rex-area-content p.rex-tx1 b{
+              background-color:#bFc9c9;
+              padding:3px;
+              color:#333;
+            }
+
+            </style>
+
             <div class="rex-area">
                 <h3 class="rex-hl2">' . $I18N->msg('yrewrite_setup') . '</h3>
                 <div class="rex-area-content">
                     <h4 class="rex-hl3">' . $I18N->msg('yrewrite_htaccess_set') . '</h4>
                     <p class="rex-tx1">' . $I18N->msg('yrewrite_htaccess_info') . '</p>
                     <p class="rex-button"><a class="rex-button" href="index.php?page=yrewrite&subpage=setup&func=htaccess">' . $I18N->msg('yrewrite_htaccess_set') . '</a></p>
-                    <h4 class="rex-hl3">' . $I18N->msg('yrewrite_info_headline') . '</h4>
+                </div>
+            </div>
+
+            <br />&nbsp;
+
+            <div class="rex-area">
+                <h3 class="rex-hl2">' . $I18N->msg('yrewrite_info_headline') . '</h3>
+                <div class="rex-area-content">
                     <p class="rex-tx1">' . $I18N->msg('yrewrite_info_text') . '</p>
                 </div>
-            </div>';
+            </div>
+
+            <br />&nbsp;
+
+            <div class="rex-area">
+                <h3 class="rex-hl2">' . $I18N->msg('yrewrite_info_seo') . '</h3>
+                <div class="rex-area-content">
+                    <p class="rex-tx1">' . $I18N->msg('yrewrite_info_seo_text') . '
+
+                      <br /><br />'.highlight_string('<?php
+  $seo = new rex_yrewrite_seo();
+  echo $seo->getTitleTag();
+  echo $seo->getDescriptionTag();
+?>',true).'
+                    </p>
+                </div>
+            </div>
+
+            <br />&nbsp;
+
+            <div class="rex-area">
+                <h3 class="rex-hl2">' . $I18N->msg('yrewrite_info_sitemaprobots') . '</h3>
+                <table class="rex-table"><tr><th>Domain</th><th>Sitemap</th><th>robots.txt</th></tr>'.implode('',$domains).'</table>
+            </div>
+            ';
+
+
+
+
