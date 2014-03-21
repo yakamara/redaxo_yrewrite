c304c84d1abcf1af340ee70b3757e1605eefa2d9
Jan Kristinus <jan.kristinus@yakamara.de>
2014-03-21 17:08:06 +0100

    Weiterleitung können nun auch / in der Url haben.

--- pages/forward.inc.php
+++ pages/forward.inc.php
@@ -25,7 +25,8 @@ if ($func != '') {
 
     $xform->setValueField('select', array('status', $I18N->msg('yrewrite_forward_status'),''.$I18N->msg("yrewrite_forward_active").'=1,'.$I18N->msg("yrewrite_forward_inactive").'=0'));
     $xform->setValueField('select_sql', array('domain', $I18N->msg('yrewrite_domain') . '', 'select domain as id,domain as name from rex_yrewrite_domain where alias_domain = ""'));
-    $xform->setValidateField('preg_match', array('url', '/^[%_\.+\-a-zA-Z0-9]+$/', $I18N->msg('yrewrite_warning_chars')));
+    //$xform->setValidateField('preg_match', array('url', '@^(?<!\/)[%_\./+\-a-zA-Z0-9]+(?!\/)$@', $I18N->msg('yrewrite_warning_chars')));
+    $xform->setValidateField('preg_match', array('url', '@^([%_\.+\-a-zA-Z0-9]){1}[/%_\.+\-a-zA-Z0-9]+([%_\.+\-a-zA-Z0-9]){1}$@', $I18N->msg('yrewrite_warning_chars')));
     // $I18N->msg('yrewrite_warning_noslash')
     $xform->setValidateField('size_range', array('url', 1, 255, $I18N->msg('yrewrite_warning_nottolong')));
     $xform->setValidateField('empty', array('url', $I18N->msg('yrewrite_forward_enter_url')));
