cbbe97a3ea3c82c976e68c1d9a7149744e75fa48
Jan Kristinus <jan.kristinus@yakamara.de>
2014-03-24 17:30:32 +0100

    cache löschen - damit alte yrwrite configs das system nicht zerhauen

--- update.inc.php
+++ update.inc.php
@@ -6,3 +6,5 @@ if (rex_string::versionCompare(OOAddon::getVersion('yrewrite'), '1.1', '<=')) {
     $sql->setQuery('ALTER TABLE `rex_yrewrite_domain` ADD `clang_start` varchar(255) NOT NULL;');
 
 }
+
+rex_generateAll();
