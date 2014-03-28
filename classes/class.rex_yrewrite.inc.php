2466395dbbb66d58fb4f9071d3987ea395d5f098
Gregor Harlan <gregor.harlan@yakamara.de>
2014-03-28 17:38:21 +0100

    Sprache bei Fehlerartikel richtig setzen, ggf. raten

--- classes/class.rex_yrewrite.inc.php
+++ classes/class.rex_yrewrite.inc.php
@@ -239,6 +239,13 @@ class rex_yrewrite
 
             // no article found -> domain not found article
             $REX['ARTICLE_ID'] = $domain->getNotfoundId();
+            $REX['CUR_CLANG'] = $domain->getStartClang();
+            foreach (self::$paths['paths'][$domain->getName()][$domain->getStartId()] as $clang => $clangUrl) {
+                if ($clang != $domain->getStartClang() && 0 === strpos($url, $clangUrl)) {
+                    $REX['CUR_CLANG'] = $clang;
+                    break;
+                }
+            }
 
             return true;
         }
