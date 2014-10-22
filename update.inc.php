1f99b03ddc1f74da33c81ce033cbc265571744ca
Jan Kristinus <jan.kristinus@yakamara.de>
2014-10-22 16:35:28 +0200

    update fehler beseitigt wenn vorheriger Version <= 1.1 war

--- update.inc.php
+++ update.inc.php
@@ -1,6 +1,7 @@
 <?php
 
 if (rex_string::versionCompare(OOAddon::getVersion('yrewrite'), '1.1', '<=')) {
+    $sql = rex_sql::factory();
     $sql->setQuery('ALTER TABLE `rex_yrewrite_domain` DROP `clang`;');
     $sql->setQuery('ALTER TABLE `rex_yrewrite_domain` ADD `clangs` varchar(255) NOT NULL;');
     $sql->setQuery('ALTER TABLE `rex_yrewrite_domain` ADD `clang_start` varchar(255) NOT NULL;');
