97622ea1ba05f66fbb3b4ed1fbb33770abf955dc
Sascha Weidner <support@sioweb.de>
2016-01-05 08:07:38 +0100

    Update install.php
    Steuerfeld mit dem Kategorien ignoriert werden können.

--- install.php
+++ install.php
@@ -12,6 +12,7 @@
 
 rex_sql_table::get('rex_article')
     ->ensureColumn(new rex_sql_column('yrewrite_url', 'varchar(255)'))
+    ->ensureColumn(new rex_sql_column('yrewrite_ignore_category', 'char(1)'))
     ->ensureColumn(new rex_sql_column('yrewrite_priority', 'varchar(5)'))
     ->ensureColumn(new rex_sql_column('yrewrite_changefreq', 'varchar(10)'))
     ->ensureColumn(new rex_sql_column('yrewrite_title', 'varchar(255)'))
