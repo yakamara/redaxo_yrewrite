197668310248da5ffa7c036852a5c9762a99a0f8
Patrick Alber <p.alber@kreatif.it>
2018-03-15 11:17:22 +0100

    excluding articles from sitemap

--- lib/seo.php
+++ lib/seo.php
@@ -232,7 +232,8 @@ class rex_yrewrite_seo
 
             $paths = 0;
             $sql = rex_sql::factory();
-            $excld_cats = [];
+            $excld_cats = rex_extension::registerPoint(new rex_extension_point('YREWRITE_SITEMAP_EXCLUDE_CATEGORY_IDS', array()));;
+            $excld_arts = rex_extension::registerPoint(new rex_extension_point('YREWRITE_SITEMAP_EXCLUDE_ARTICLE_IDS', array()));;
             $domain_article_id = $domain->getStartId();
 
             if (($dai = rex_article::get($domain_article_id))) {
@@ -243,6 +244,10 @@ class rex_yrewrite_seo
                 foreach ($domain->getClangs() as $clang_id) {
 
                     if (!rex_clang::get($clang_id)->isOnline()) {
+                        continue;
+                    }
+
+                    if (isset($excld_arts[$article_id]) && in_array($clang_id,$excld_arts[$article_id])) {
                         continue;
                     }
 
