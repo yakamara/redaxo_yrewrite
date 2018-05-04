11347f599656507c72f57a402f583c26ad28e338
Thomas Blum <thomas@redaxo.org>
2018-05-04 21:03:08 +0200

    Korrekte clangId setzen (#214)
    U.a. wurde der NotFoundArticle immer in der clangId=1 ausgeliefert

--- lib/yrewrite.php
+++ lib/yrewrite.php
@@ -356,7 +356,7 @@ class rex_yrewrite
         $structureAddon->setProperty('article_id', $domain->getNotfoundId());
         rex_clang::setCurrentId($domain->getStartClang());
         foreach (self::$paths['paths'][$domain->getName()][$domain->getStartId()] as $clang => $clangUrl) {
-            if ($clang != $domain->getStartClang()  && $clangUrl > 0 && 0 === strpos($url, $clangUrl)) {
+            if ($clang != $domain->getStartClang() && $clangUrl != '' && 0 === strpos($url, $clangUrl)) {
                 rex_clang::setCurrentId($clang);
                 break;
             }
