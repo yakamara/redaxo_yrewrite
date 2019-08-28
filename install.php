219a31a7d6c7c2e9dc79275881690147a4c13ccd
Jan Kristinus <jan.kristinus@yakamara.de>
2019-08-28 16:48:49 +0200

    utf8mb4 Umstellung

--- install.php
+++ install.php
@@ -10,11 +10,11 @@
  */
 
 rex_sql_table::get(rex::getTable('article'))
-    ->ensureColumn(new rex_sql_column('yrewrite_url', 'varchar(255)'))
-    ->ensureColumn(new rex_sql_column('yrewrite_canonical_url', 'varchar(255)'))
+    ->ensureColumn(new rex_sql_column('yrewrite_url', 'text'))
+    ->ensureColumn(new rex_sql_column('yrewrite_canonical_url', 'text'))
     ->ensureColumn(new rex_sql_column('yrewrite_priority', 'varchar(5)'))
     ->ensureColumn(new rex_sql_column('yrewrite_changefreq', 'varchar(10)'))
-    ->ensureColumn(new rex_sql_column('yrewrite_title', 'varchar(255)'))
+    ->ensureColumn(new rex_sql_column('yrewrite_title', 'varchar(191)'))
     ->ensureColumn(new rex_sql_column('yrewrite_description', 'text'))
     ->ensureColumn(new rex_sql_column('yrewrite_index', 'tinyint(1)'))
     ->alter()
