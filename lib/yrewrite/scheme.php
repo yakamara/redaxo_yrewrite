5c7ea892f99651a19ee6bfb30ba5a91e68bd73dc
Gregor Harlan <mail@gh01.de>
2020-03-22 13:10:30 +0100

    Auch bei nur einer Sprache diese mit in URL (wenn clang_start_hidden=0)

--- lib/yrewrite/scheme.php
+++ lib/yrewrite/scheme.php
@@ -36,7 +36,7 @@ class rex_yrewrite_scheme
      */
     public function getClang($clang, rex_yrewrite_domain $domain)
     {
-        if (count($domain->getClangs()) <= 1 || $domain->isStartClangHidden() && $clang == $domain->getStartClang()) {
+        if ($domain->isStartClangHidden() && $clang == $domain->getStartClang()) {
             return '';
         }
 
