7e61bcb96d78de59c746484f63cbff06493049a6
Tobias Krais <tk@design-to-use.de>
2022-06-20 07:10:57 +0200

    Redaxo style
    Code nach dem Vorbild es Media Managers angepasst

--- install.php
+++ install.php
@@ -81,8 +81,16 @@ $c->setQuery('ALTER TABLE `' . rex::getTable('yrewrite_forward') . '` CONVERT TO
 // Add media manager type
 $c->setQuery("SELECT * FROM ". rex::getTable('media_manager_type') ." WHERE name = 'yrewrite_seo_image'");
 if($c->getRows() == 0) {
-	$c->setQuery("INSERT INTO ". rex::getTable('media_manager_type') ." (`status`, `name`, `description`) VALUES
-		(0, 'yrewrite_seo_image', 'YRewrite SEO Vorschaubild für Sitemap und Open Graph Tags');");
+	$data = ['status' => 0, 'name' => 'yrewrite_seo_image', 'description' => 'YRewrite SEO Vorschaubild für Sitemap und Open Graph Tags'];
+	$sql = rex_sql::factory();
+	$sql->setTable(rex::getTable('media_manager_type'));
+    $sql->addRecord(static function (rex_sql $record) use ($data) {
+        $record
+            ->setValues($data)
+            ->addGlobalCreateFields()
+            ->addGlobalUpdateFields();
+    });
+	$sql->insertOrUpdate();
 }
 
 rex_delete_cache();
