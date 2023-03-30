d11505bca5b313ec3b68a46a562b9055cbf6950a
Gregor Harlan <mail@gh01.de>
2023-03-31 00:14:41 +0200

    update

--- lib/yrewrite/seo.php
+++ lib/yrewrite/seo.php
@@ -147,22 +147,15 @@ class rex_yrewrite_seo
 
     public function getTitle()
     {
-        $title_scheme = htmlspecialchars_decode(trim($this->domain->getTitle()));
-        if ('' == $title_scheme) {
-            $title_scheme = self::$title_scheme_default;
+        $title = $this->article->getValue(self::$meta_title_field);
+        if ('' == $title) {
+            $title = htmlspecialchars_decode(trim($this->domain->getTitle()));
         }
-    
-        $title = $title_scheme;
-        $ytitle = '';
-        
-        if ($this->article && '' != $this->article->getValue(self::$meta_title_field)) {
-            $title = $ytitle = $this->article->getValue(self::$meta_title_field);
+        if ('' == $title) {
+            $title = self::$title_scheme_default;
         }
-        if ('' == $ytitle) {
-            $ytitle = $this->article->getValue('name');
-        }
-        
-        $title = str_replace('%T', $ytitle, $title);
+
+        $title = str_replace('%T', $this->article->getValue('name'), $title);
         $title = str_replace('%SN', rex::getServerName(), $title);
 
         return $this->cleanString($title);
